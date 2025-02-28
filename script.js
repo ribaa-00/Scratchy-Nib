7b3e15a5031a5d6b7bb9a20c06fa07d491f89311
riba <ribasuleman97@gmail.com>
2025-02-28 20:30:14 +0500

    first data

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+(function () {
+    'use strict'
+    var form = document.getElementById('contactForm')
+
+    form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+        form.classList.add('was-validated')
+    }, false)
+})()
